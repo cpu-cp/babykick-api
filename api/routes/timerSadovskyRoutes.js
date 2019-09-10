55628dad0be731ae16c37919108634f503f6f9e1
chompusama <chadaporn.p@kkumai.com>
2019-09-11 01:37:52 +0700

    fixed timer sdk

--- api/routes/timerSadovskyRoutes.js
+++ api/routes/timerSadovskyRoutes.js
@@ -165,7 +165,8 @@ router.post("/", (req, res, next) => {
                     dataCollection.updateOne({ line_id: req.body.line_id }, {
                         $set: {
                             timer_status: "timeout",
-                            sdk_status: "disable"
+                            sdk_status: "disable",
+                            extra: 'disable'
                         },
                     }, function (err, docs) {
                         console.log(err)
