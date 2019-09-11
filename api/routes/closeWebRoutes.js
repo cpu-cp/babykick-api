f84193b0ccab7582bc6432c69b39a2d1594ba3cf
chompusama <chadaporn.p@kkumai.com>
2019-09-12 02:26:39 +0700

    add link in close web api

--- api/routes/closeWebRoutes.js
+++ api/routes/closeWebRoutes.js
@@ -22,48 +22,104 @@ router.post("/", (req, res, next) => {
     if (req.body.status_web == 'exit') {
         res.status(200).json({ message: 'web is closed' });
 
-        / push message to line */
-        const client = new line.Client({
-            channelAccessToken: 'SCtu4U76N1oEXS3Ahq1EX9nBNkrtbKGdn8so1vbUZaBIXfTlxGqMldJ3Ego3GscxKGUB7MlfR3DHtTbg6hrYPGU9reSTBcCSiChuKmDCMx4FTtIPXzivaYUi3I6Yk1u/yF5k85Le0IUFrkBNxaETxFGUYhWQfeY8sLGRXgo3xvw='
-        });
-        const message = [
-            {
-                type: 'text',
-                text: 'คุณแม่ยังนับไม่ครบเลย อย่าลืมกลับมานับต่อนะคะ 😊'
-            },
-            {
-                type: "flex",
-                altText: "นับลูกดิ้นต่อ",
-                contents: {
-                    type: "bubble",
-                    body: {
-                        type: "box",
-                        layout: "vertical",
-                        contents: [
-                            {
-                                type: "button",
-                                style: "primary",
-                                height: "sm",
-                                action: {
-                                    type: "uri",
-                                    label: "นับลูกดิ้นต่อ",
-                                    uri: "line://app/1606482498-mYZjO7zo"
-                                },
-                                color: "#dd8cc9"
+        dataCollection.findOne({ line_id: req.body.line_id })
+            .exec()
+            .then(docs => {
+                if (docs.counting[(docs.counting.length) - 1].count_type == 'CTT') {
+                    / push message to line */
+                    const client = new line.Client({
+                        channelAccessToken: 'SCtu4U76N1oEXS3Ahq1EX9nBNkrtbKGdn8so1vbUZaBIXfTlxGqMldJ3Ego3GscxKGUB7MlfR3DHtTbg6hrYPGU9reSTBcCSiChuKmDCMx4FTtIPXzivaYUi3I6Yk1u/yF5k85Le0IUFrkBNxaETxFGUYhWQfeY8sLGRXgo3xvw='
+                    });
+                    const message = [
+                        {
+                            type: 'text',
+                            text: 'คุณแม่ยังนับไม่ครบเลย อย่าลืมกลับมานับต่อนะคะ 😊'
+                        },
+                        {
+                            type: "flex",
+                            altText: "นับลูกดิ้นแบบ Count to ten ต่อ",
+                            contents: {
+                                type: "bubble",
+                                body: {
+                                    type: "box",
+                                    layout: "vertical",
+                                    contents: [
+                                        {
+                                            type: "button",
+                                            style: "primary",
+                                            height: "sm",
+                                            action: {
+                                                type: "uri",
+                                                label: "นับลูกดิ้นแบบ Count to ten ต่อ",
+                                                uri: "line://app/1606482498-mYZjO7zo"
+                                            },
+                                            color: "#dd8cc9"
+                                        }
+                                    ]
+                                }
                             }
-                        ]
-                    }
+                        }
+                    ]
+                    client.pushMessage(req.body.line_id, message)
+                        .then(() => {
+                            console.log('push message verify done!')
+                        })
+                        .catch((err) => {
+                            console.log(err);   // error when use fake line id 
+                        });
                 }
-            }
-        ]
+                else {
+                    / push message to line */
+                    const client = new line.Client({
+                        channelAccessToken: 'SCtu4U76N1oEXS3Ahq1EX9nBNkrtbKGdn8so1vbUZaBIXfTlxGqMldJ3Ego3GscxKGUB7MlfR3DHtTbg6hrYPGU9reSTBcCSiChuKmDCMx4FTtIPXzivaYUi3I6Yk1u/yF5k85Le0IUFrkBNxaETxFGUYhWQfeY8sLGRXgo3xvw='
+                    });
+                    const message = [
+                        {
+                            type: 'text',
+                            text: 'คุณแม่ยังนับไม่ครบเลย อย่าลืมกลับมานับต่อนะคะ 😊'
+                        },
+                        {
+                            type: "flex",
+                            altText: "นับลูกดิ้นแบบ Sadovsky ต่อ",
+                            contents: {
+                                type: "bubble",
+                                body: {
+                                    type: "box",
+                                    layout: "vertical",
+                                    contents: [
+                                        {
+                                            type: "button",
+                                            style: "primary",
+                                            height: "sm",
+                                            action: {
+                                                type: "uri",
+                                                label: "นับลูกดิ้นแบบ Sadovsky ต่อ",
+                                                uri: "line://app/1606482498-lJ8JkE6d"
+                                            },
+                                            color: "#dd8cc9"
+                                        }
+                                    ]
+                                }
+                            }
+                        }
+                    ]
 
-        client.pushMessage(req.body.line_id, message)
-            .then(() => {
-                console.log('push message verify done!')
-            })
-            .catch((err) => {
-                console.log(err);   // error when use fake line id 
+                    client.pushMessage(req.body.line_id, message)
+                        .then(() => {
+                            console.log('push message verify done!')
+                        })
+                        .catch((err) => {
+                            console.log(err);   // error when use fake line id 
+                        });
+                }
+            }).catch(err => {
+                console.log(err)
+                res.json({
+                    message: 'error',
+                });
             });
+
+
     }
     else {
         res.status(200).json({ message: 'web is openning' });          // that account is exists return false string
