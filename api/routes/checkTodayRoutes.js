244781160c01d497fad519eb35a71f06fdbae8fc
chompusama <chadaporn.p@kkumai.com>
2019-09-19 18:17:46 +0700

    fixed check today

--- api/routes/checkTodayRoutes.js
+++ api/routes/checkTodayRoutes.js
@@ -24,54 +24,59 @@ router.post("/:lineId", (req, res, next) => {
         .exec()
         .then(docs => {
 
-            current = docs.counting[(docs.counting.length) - 1].date;
-            let todayDate = today.toLocaleDateString();
-            let currentDate = current.toLocaleDateString();
+            if (docs.counting.length == 0) {
+                res.status(200).json({
+                    add: true
+                });
+            }
+            else {
+                current = docs.counting[(docs.counting.length) - 1].date;
+                let todayDate = today.toLocaleDateString();
+                let currentDate = current.toLocaleDateString();
 
+                if (todayDate == currentDate) {
+                    console.log('+++++ ' + currentDate);
+                    console.log('++++++ today +++++' + todayDate)
 
-            if (todayDate == currentDate) {
+                    if (docs.extra == 'ctt') {   // mom be able to re-counting
+                        res.status(200).json({
+                            add: true
+                        });
+                    }
+                    else {
+                        res.status(401).json({
+                            add: false,
+                        });
 
-                console.log('+++++ ' + currentDate);
-                console.log('++++++ today +++++' + todayDate)
-
-                if (docs.extra == 'ctt') {   // mom be able to re-counting
+                        / push messsage to line */
+                        const client = new line.Client({
+                            channelAccessToken: 'U/6mbMtkbHi+PGkWqm50UCz4tpPV2pVgMRKyLM5ewyp4QhDIqv+wiyFuKR4Vof8Gh09nAGnpbOUhuk3mrkGULX68GRZ/L3rTT/txU6+eqMWE/7DUuzKTOUH1jayB7XPCJGtXQGxQgSxjkHr1+aAV4gdB04t89/1O/w1cDnyilFU='
+                        });
+                        const message = [
+                            {
+                                type: 'text',
+                                text: 'วันนี้คุณแม่นับลูกดิ้นแล้วค่ะ'
+                            },
+                            {
+                                type: "sticker",
+                                packageId: 3,
+                                stickerId: 181
+                            }
+                        ];
+                        client.pushMessage(req.params.lineId, message)
+                            .then(() => {
+                                console.log('check today : push message done!')
+                            })
+                            .catch((err) => {
+                                console.log(err);
+                            });
+                    }
+                }
+                else {
                     res.status(200).json({
                         add: true
                     });
                 }
-                else {
-                    res.status(401).json({  
-                        add: false,
-                    });
-
-                    / push messsage to line */
-                    const client = new line.Client({
-                        channelAccessToken: 'U/6mbMtkbHi+PGkWqm50UCz4tpPV2pVgMRKyLM5ewyp4QhDIqv+wiyFuKR4Vof8Gh09nAGnpbOUhuk3mrkGULX68GRZ/L3rTT/txU6+eqMWE/7DUuzKTOUH1jayB7XPCJGtXQGxQgSxjkHr1+aAV4gdB04t89/1O/w1cDnyilFU='
-                    });
-                    const message = [
-                        {
-                            type: 'text',
-                            text: 'วันนี้คุณแม่นับลูกดิ้นแล้วค่ะ'
-                        },
-                        {
-                            type: "sticker",
-                            packageId: 3,
-                            stickerId: 181
-                        }
-                    ];
-                    client.pushMessage(req.params.lineId, message)
-                        .then(() => {
-                            console.log('check today : push message done!')
-                        })
-                        .catch((err) => {
-                            console.log(err);
-                        });
-                }
-            }
-            else {
-                res.status(200).json({
-                    add: true
-                });
             }
 
         }).catch(err => {
