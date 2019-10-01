a6d554560a068a1c4596299dc42e96e6e431a38f
chompusama <chadaporn.p@kkumai.com>
2019-10-01 20:45:05 +0700

    change millisecond in timer ctt

--- api/routes/timerCountotenRoutes.js
+++ api/routes/timerCountotenRoutes.js
@@ -31,47 +31,6 @@ router.post("/", (req, res, next) => {
                     message: 'line id is invalid',
                 });
 
-                // / push message to line */
-                // const client = new line.Client({
-                //     channelAccessToken: 'SCtu4U76N1oEXS3Ahq1EX9nBNkrtbKGdn8so1vbUZaBIXfTlxGqMldJ3Ego3GscxKGUB7MlfR3DHtTbg6hrYPGU9reSTBcCSiChuKmDCMx4FTtIPXzivaYUi3I6Yk1u/yF5k85Le0IUFrkBNxaETxFGUYhWQfeY8sLGRXgo3xvw='
-                // });
-                // const message = [
-                //     {
-                //         type: 'text',
-                //         text: 'คุณแม่ต้องลงทะเบียนก่อนใช้งานการนับลูกดิ้นนะคะ'
-                //     },
-                //     {
-                //         type: "flex",
-                //         altText: "ลงทะเบียนคุณแม่",
-                //         contents: {
-                //             type: "bubble",
-                //             body: {
-                //                 type: "box",
-                //                 layout: "vertical",
-                //                 contents: [
-                //                     {
-                //                         type: "button",
-                //                         style: "primary",
-                //                         height: "sm",
-                //                         action: {
-                //                             type: "uri",
-                //                             label: "ลงทะเบียนคุณแม่",
-                //                             uri: "line://app/1606482498-VJdOoZXR"
-                //                         },
-                //                         color: "#dd8cc9"
-                //                     }
-                //                 ]
-                //             }
-                //         }
-                //     }
-                // ]
-                // client.pushMessage(req.body.line_id, message)
-                //     .then(() => {
-                //         console.log('push message go to ctt done!')
-                //     })
-                //     .catch((err) => {
-                //         console.log(err);   // error when use fake line id 
-                //     });
             }
             else {
 
@@ -349,9 +308,9 @@ router.post("/", (req, res, next) => {
                         });
                     });
 
-            }, 43200000);
+            }, 21600000);
 
-        }, 43200000);   // 43200000 = 12 hr , 21000 = 20 sec , 63000 = 1 min
+        }, 21600000);   // 43200000 = 12 hr , 21000 = 20 sec , 63000 = 1 min
 
     }
 });
@@ -372,3 +331,47 @@ module.exports = router;
                     //         console.log('remove array');
                     //     });
                     // });
+
+
+
+// / push message to line */
+                // const client = new line.Client({
+                //     channelAccessToken: 'SCtu4U76N1oEXS3Ahq1EX9nBNkrtbKGdn8so1vbUZaBIXfTlxGqMldJ3Ego3GscxKGUB7MlfR3DHtTbg6hrYPGU9reSTBcCSiChuKmDCMx4FTtIPXzivaYUi3I6Yk1u/yF5k85Le0IUFrkBNxaETxFGUYhWQfeY8sLGRXgo3xvw='
+                // });
+                // const message = [
+                //     {
+                //         type: 'text',
+                //         text: 'คุณแม่ต้องลงทะเบียนก่อนใช้งานการนับลูกดิ้นนะคะ'
+                //     },
+                //     {
+                //         type: "flex",
+                //         altText: "ลงทะเบียนคุณแม่",
+                //         contents: {
+                //             type: "bubble",
+                //             body: {
+                //                 type: "box",
+                //                 layout: "vertical",
+                //                 contents: [
+                //                     {
+                //                         type: "button",
+                //                         style: "primary",
+                //                         height: "sm",
+                //                         action: {
+                //                             type: "uri",
+                //                             label: "ลงทะเบียนคุณแม่",
+                //                             uri: "line://app/1606482498-VJdOoZXR"
+                //                         },
+                //                         color: "#dd8cc9"
+                //                     }
+                //                 ]
+                //             }
+                //         }
+                //     }
+                // ]
+                // client.pushMessage(req.body.line_id, message)
+                //     .then(() => {
+                //         console.log('push message go to ctt done!')
+                //     })
+                //     .catch((err) => {
+                //         console.log(err);   // error when use fake line id 
+                //     });
