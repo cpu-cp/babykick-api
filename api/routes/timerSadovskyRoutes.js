2bdac57f6955670631ef930f6f6b16465d6c8b4b
chompusama <chadaporn.p@kkumai.com>
2019-10-08 02:30:51 +0700

    change extra time for th timezone

--- api/routes/timerSadovskyRoutes.js
+++ api/routes/timerSadovskyRoutes.js
@@ -512,13 +512,7 @@ router.post("/", (req, res, next) => {
                         else if (docs.counting[(docs.counting.length) - 1].sdk_all_meal < 10) { // go to extra
 
                             var d = new Date(Date.now());
-                            var hr = (7 + d.getHours()) % 24;
-                            var min = d.getMinutes();
-                            var sec = d.getSeconds();
-                            if (hr < 10) hr = '0' + hr;
-                            if (min < 10) min = '0' + min;
-                            if (sec < 10) sec = '0' + sec;
-                            var time = hr.toString() + ':' + min.toString() + ':' + sec.toString();
+                            let time = d.toLocaleTimeString('en-TH', { hour12: false });
 
                             dataCollection.findOneAndUpdate({ line_id: req.body.line_id, 'counting._did': _dids }, {
                                 $set: {
